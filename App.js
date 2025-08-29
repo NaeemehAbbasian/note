ac9cc2b0214d5dd3777ac4701393bffc2a84fc37
naeemeh <refreshsho@gmail.com>
2025-08-29 14:10:43 +0330

    being able to save in local storage (but it can't delete yet)

--- App.js
+++ App.js
@@ -1,28 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btnEl = document.getElementById("btn");
+  const noteInput = document.getElementById("note-input");
+  const noteContainer = document.getElementById("note-container");
 
-document.addEventListener ( 'DOMContentLoaded' , () => { 
-    const btnEl = document.getElementById('btn');
-    const noteInput = document.getElementById('note-input');
-    const noteContainer = document.getElementById('container');
-
-    btnEl.addEventListener('click',()=> {
-        const noteText = noteInput.value.trim ();
-        if (noteText !== '') {
-            createNoteElement (noteText);
-            noteInput.value = '';
-        }
+  loadNotes();
+  function loadNotes() {
+    const notes = JSON.parse(localStorage.getItem("notes")) || [];
+    notes.forEach((note) => {
+      createNoteElement(note);
     });
+  }
 
-    function createNoteElement(text) {
-        const noteItem = document.createElement('div');
-        noteItem.className = 'note-item';
-
-        const noteContent = document.createElement('p');
-        noteContent.className = 'note-content';
-        noteContent.textContent = text;
-           noteItem.appendChild(noteContent);
-        noteContainer.appendChild(noteItem);
+  btnEl.addEventListener("click", () => {
+    const noteText = noteInput.value.trim();
+    if (noteText !== "") {
+      createNoteElement(noteText);
+      saveNote(noteText);
+      noteInput.value = "";
     }
+  });
 
+  function createNoteElement(text) {
+    const noteItem = document.createElement("div");
+    noteItem.className = "note";
 
-
-
+    const noteContent = document.createElement("p");
+    noteContent.className = "note-content";
+    noteContent.textContent = text;
+    noteItem.appendChild(noteContent);
+    noteContainer.appendChild(noteItem);
+  }
+  function saveNote(text) {
+    const notes = JSON.parse(localStorage.getItem("notes")) || [];
+    notes.push(text);
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }
+});
